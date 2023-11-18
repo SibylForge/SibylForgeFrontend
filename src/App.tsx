c00f6da8052c050a7da7c4a2c1bc354e1818a698
Hanz <e102777tw@gmail.com>
2023-11-18 23:46:09 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ThreeJsCanvas from "./component/Canvas";
+
+function App() {
+  return (
+    <>
+      <ThreeJsCanvas />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Box.tsx
@@ -0,0 +1,31 @@
+import { useFrame, ReactThreeFiber } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import { Mesh } from "three";
+
+export interface IBoxProps {
+  position: ReactThreeFiber.Vector3;
+}
+
+function Box(props: IBoxProps) {
+  const ref = useRef<Mesh>(null!);
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+
+  useFrame((_, delta) => (ref.current.rotation.x += delta));
+
+  return (
+    <mesh
+      position={props.position}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={() => click(!clicked)}
+      onPointerOver={(event) => (event.stopPropagation(), hover(true))}
+      onPointerOut={() => hover(false)}
+    >
+      <sphereGeometry args={[1, 100, 100]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
+
+export default Box;
